6775859be2aab58bb5a33b025a9c4f1ff0922066
Randi Schreiner <mimirandi21@gmail.com>
2020-10-06 11:50:34 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+#define a class
+class Dog
+    #setter method for name
+    def name=(name)
+        @name = name
+    end
+    #getter method for name
+    def name
+        @name
+    end
+
+    #setter method for breed
+    def breed=(breed)
+        @breed = breed
+    end
+    #getter method for breed
+    def breed
+        @breed
+    end
+end
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+#create a Person class
+class Person
+    #setter method for name
+    def name=(name)
+        @name = name     
+    end
+    #getter method for name
+    def name
+        @name
+    end
+    #setter method for job
+    def job=(job)
+        @job = job
+    end
+    #getter method for job
+    def job
+        @job
+    end
+end
